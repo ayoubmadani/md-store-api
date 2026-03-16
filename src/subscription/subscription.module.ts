8fae54fcbdb935e03260a55529631da07e782a62
ayoubmadani <ayoubbansalah123456@gmail.com>
2026-03-16 05:10:00 +0100

    Fix: sub 5

--- src/subscription/subscription.module.ts
+++ src/subscription/subscription.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SubscriptionService } from './subscription.service';
 import { SubscriptionController } from './subscription.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -10,13 +10,12 @@ import { PlansService } from './plan.service';
 import { FeaturesEntity } from './entities/features.entity';
 
 @Module({
-  controllers: [SubscriptionController,PlansController],
-  providers: [SubscriptionService,PlansService],
-  exports: [SubscriptionService,PlansService],
+  controllers: [SubscriptionController, PlansController],
+  providers: [SubscriptionService, PlansService],
+  exports: [SubscriptionService, PlansService],
   imports: [
-    TypeOrmModule.forFeature([Plan,Subscription,FeaturesEntity]),
-    PaymentModule,
+    TypeOrmModule.forFeature([Plan, Subscription, FeaturesEntity]),
+    forwardRef(() => PaymentModule), // مهم
   ],
-
 })
 export class SubscriptionModule {}
